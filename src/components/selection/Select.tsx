5e1708a6a3d0180f902f412b6e99075d36343702
xTavi <tavitavi62@gmail.com>
2021-07-04 03:00:12 +0300

    Now UserType is saved in local storage and changed when you go on profile page

--- src/components/selection/Select.tsx
+++ src/components/selection/Select.tsx
@@ -4,9 +4,12 @@ import {useHistory} from 'react-router-dom'
 function Select() {
    const history = useHistory()
 
-   function handleClick() {
-      history.push('/Information')
+   function handleClick(userType: string) {
+      localStorage.setItem('userType', userType)
+      history.push('/Information');
    }
+
+
    return (
       <div className='select__container'>
          <Navigation />
@@ -15,7 +18,7 @@ function Select() {
             <div className="wrapper__container">
                <h2 className="select__main-heading">What are you?</h2>
                <div className='select'>
-                  <div className='select__investor' onClick={() => handleClick()}>
+                  <div className='select__investor' onClick={() => handleClick('investor')}>
                      <h3 className="select__heading">Investor</h3>
                      <div className="select__image-container">
                         <img src={process.env.PUBLIC_URL + 'assets/investor.svg'} alt="investor"
@@ -23,7 +26,7 @@ function Select() {
                      </div>
                      <p className="select__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, veritatis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore assumenda maxime eaque!</p>
                   </div>
-                  <div className='select__entrepreneur' onClick={() => handleClick()}>
+                  <div className='select__entrepreneur' onClick={() => handleClick('entrepreneur')}>
                      <h3 className="select__heading">Entrepreneur</h3>
                      <div className="select__image-container">
                         <img src={process.env.PUBLIC_URL + 'assets/entrepreneur.svg'} alt="entrepreneur"
